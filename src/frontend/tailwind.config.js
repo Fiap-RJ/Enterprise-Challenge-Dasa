d8e91bc3b8dbfe6d3dac7681f9e2f270e30b8af9
NAVasconcelos <vasconcelos.nathalia.a@gmail.com>
2026-05-27 22:53:50 -0300

    ajustado o frontend para o estilo da genera

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Ubuntu', 'sans-serif'], // Define a Ubuntu como fonte principal
+      },
+      colors: {
+        genera: {
+          roxo: '#2D004B',       // Roxo escuro para textos e cabeçalho
+          magenta: '#E6007E',    // Magenta oficial dos botões
+          magentahover: '#C5006A' // Magenta um pouco mais escuro para o hover
+        }
+      }
+    },
   },
   plugins: [],
 }
